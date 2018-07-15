8f1c69d49fa2fba23c6958486c1e488637ec9e1d
Dani Teubl <teubl.dani@gmail.com>
2018-07-15 19:31:24 +0200

    Regex added, input read fine

--- getver.cpp
+++ getver.cpp
@@ -4,15 +4,29 @@
  */
 
 #include <iostream>
+#include <regex>
+#include <string>
 
 int main(int argc, char *argv[] ) {
 
+    try{
     std::cout << argc << std::endl;
 
     for(int i=0; i<argc; ++i)
         std::cout << argv[i] << "\t";
 
     std::cout << std::endl;;
+
+//     std::string gitVer=argv[1];
+    std::string gitVer{argv[1]};
+
+    std::cout << gitVer << std::endl;
+
+    std::cout << std::endl;;
+    }
+    catch(const char* e)  {
+        std::cout << e << std::endl;
+    }
 
     return 0;
 }
