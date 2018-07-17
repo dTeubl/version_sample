e2c0b333cd67d00eb888844acba00bd989a1336c
Dani Teubl <teubl.dani@gmail.com>
2018-07-18 00:31:33 +0200

    New line is created, just replace needed

--- getver.cpp
+++ getver.cpp
@@ -4,12 +4,26 @@
  */
 
 #include <iostream>
+#include <fstream>
 #include <regex>
 #include <string>
 
 int main(int argc, char *argv[] ) {
 
-    try{
+    unsigned int MAGIC = 0;
+////
+/////
+/////
+
+
+
+
+///
+///
+//
+
+    try{ 
+
     std::cout << argc << std::endl;
 
     if( argc < 3 )
@@ -38,9 +52,50 @@ int main(int argc, char *argv[] ) {
     
     std::cout << std::endl;
 
+    MAGIC = static_cast<unsigned int>(std::stoi(match.str(2)));
+    // Now the MAGIC part, We should rewrite the value of the MAGIC variable!!
+    
+   
+    std::fstream file{"getver.cpp",std::ios::in | std::ios::out };
+    std::string line;
+    std::string reg_magic{"(([ ]*[a-zA-Z]*)*)(MAGIC)( = )([0-9]+)(.)*"};
+//     std::string reg_magic{"(.)*(MAGIC)( = )([0-9]+)(.)*"}; // <<<----this works
+    std::regex base{reg_magic};
+    std::smatch res{};
+    // Now read until we find the MAGIC, than replace!
+    
+    std::cout << "reading file: " << "getver.cpp" << std::endl;
+    std::string vers{};
 
+    while( std::getline(file,line)) {
+        if( std::regex_match(line,res,base) ) {
 
+        std::cout << res.size() << std::endl;
+        for( size_t i=1; i<res.size(); i++)
+            std::cout << "res " << i << ": "<< res.str(i) << ";\n";
+        
+        std::cout << std::endl;
 
+        std::cout << line << std::endl;
+        std::string nl{};
+        nl+=res.str(1);
+        nl+=res.str(3);
+        nl+=res.str(4);
+        nl+=match.str(2);
+        nl+=res.str(6);
+
+        std::cout << "new line is: " << nl << std::endl;
+
+        if( res.str(3) == "MAGIC" ) {
+            std::cout << "We have it now!" << std::endl;
+//             file << "hahahahahah";  //<<--- this also works
+            break;
+        }
+
+        } // We foun something!
+    } // while( )
+
+    file.close();
 
     }
     catch(const char* e)  {
