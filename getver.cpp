5ee0a8c53cdb54462eabebf8596d443ae065056a
Dani Teubl <teubl.dani@gmail.com>
2018-07-24 13:18:35 +0800

    Temporary buffer used. sollution works.
    It has some dirty methods in it yet. For Safety reasons,
    it could be better to put the data from the buffer to a
    temporary file, then invoking system commands to switch
    the files and delete the temporary one.

--- getver.cpp
+++ getver.cpp
@@ -16,8 +16,115 @@
 #include <string>
 #include "fwid.h"
 
+
+// The next three function do the main job, and rewrites the right line 
+// in the file buffer. It is a dirty sollution, cus every function rewrites 
+// every line, even if it is not needed. 
+
+/**
+ * Finds the line which contain the tag_ value and refresh the whole line
+ * l: current line of the file
+ * newTag: new tag value to be written into file
+ */
+std::string UpdateTag( std::string l, std::string newTag ) {
+    // Check the new tag for validity
+    std::string rS = l;
+
+    std::regex rTag{std::string{"(v[0-9]*)(.)([0-9]*)"}};
+    if( !std::regex_match(newTag,rTag) ) {
+        throw("Incorrect new tag value!");
+    }
+
+    // triggers for the line with the tag_ in it, and cut it into pieces    
+    std::string reg_tag{"([ ]*[a-zA-Z]*)(.)(tag_ = \")((v[0-9]*)(.)([0-9]*))(\")(.)*"};
+    std::regex base_reg{reg_tag};
+    std::smatch res;
+
+    if( std::regex_match(l,res,base_reg) ) {
+//         std::cout << "found it!" << std::endl;
+//         std::cout << "current line is:\n" << l << std::endl;
+
+        rS = std::string{};
+        rS+=res.str(1);
+        rS+=res.str(2);
+        rS+=res.str(3);
+        rS+=newTag;
+        rS+=res.str(8);
+        rS+=res.str(9);
+
+//         std::cout << "New Line should be:\n" << rS << std::endl;
+
+    }
+    return rS;
+}
+
+/**
+ * Finds the line which contain the branch_ value and refresh the whole line
+ * l: current line of the file
+ * newBranch: new Branch value to be written into file
+ */
+std::string  UpdateBracnh( std::string l, std::string newBranch ) {
+    std::string rS=l;
+    // triggers for the line with the branch_ in it, and cut it into pieces    
+    std::string reg{"([ ]*[a-zA-Z]*)(.)(branch_ = \")([a-zA-Z0-9]*)(\")(.)*"}; 
+    std::regex base_reg{reg};
+    std::smatch res;
+
+    if( std::regex_match(l,res,base_reg) ) {
+        std::cout << "found it!" << std::endl;
+        std::cout << "current line is:\n" << l << std::endl;
+        rS = std::string{};
+
+        rS+=res.str(1);
+        rS+=res.str(2);
+        rS+=res.str(3);
+        rS+=newBranch;
+        rS+=res.str(5);
+        rS+=res.str(6);
+
+        std::cout << "New Line should be:\n" << rS << std::endl;
+
+    }
+    return rS;
+}
+
+/**
+ * Finds the line which contain the buildNum_ value and refresh the whole line
+ * f: file pointer
+ * l: current line of the file
+ * pw: writing pointer of the file - where this function will srite
+ * buildNum: new nuild number value to be written into file
+ */
+std::string UpdateBuilNum( std::string l, std::string buildNum ) {
+    std::string rS = l;
+    // triggers for the line with the branch_ in it, and cut it into pieces    
+    std::string reg{"([ ]*[a-zA-Z]*)(.)(buildNum_ = )([0-9]*)(.)*"};
+    std::regex base_reg{reg};
+    std::smatch res;
+
+    if( std::regex_match(l,res,base_reg) ) {
+        std::cout << "found it!" << std::endl;
+        std::cout << "current line is:\n" << l << std::endl;
+
+        rS=std::string{};
+        rS+= res.str(1);
+        rS+= res.str(2);
+        rS+= res.str(3);
+        rS+= buildNum;
+        rS+= res.str(5);
+
+        std::cout << "New Line should be:\n" << rS << std::endl;
+
+    }
+    return rS;
+}
+
+
+
+
+
 int main(int argc, char *argv[] ) {
-    int buildNum=0;
+    std::string buildNum{};
 
     try{ 
 
@@ -49,7 +156,7 @@ int main(int argc, char *argv[] ) {
     
     std::cout << std::endl;
 
-    buildNum = static_cast<unsigned int>(std::stoi(match.str(2)));
+    buildNum = std::string{match.str(2)};
 
     
 //=========================== FWID modification ===============================
@@ -73,17 +180,31 @@ int main(int argc, char *argv[] ) {
     std::smatch res;
     std::string line;
 
-    std::fstream file2 {"fwid.cpp",std::ios::in | std::ios::out };
+    std::fstream file {"fwid.cpp",std::ios::in };
+    std::vector<std::string> tempF{};
 
-    while( std::getline(file2,line)) {
-
-        std::cout << line << std::endl;
-
-        if( std::regex_match(line,res,reg) ) {
-
-            std::cout << "found it!" << std::endl;
-        }
+    while( std::getline(file,line)) {
+        tempF.push_back(line);
     }
+    
+    file.close();
+
+    // change the data inside the buffer
+    for( auto &l : tempF ) {
+//         std:: cout << l << std::endl;
+        l = UpdateTag(l, std::string{argv[1]});
+        l = UpdateBracnh(l, std::string{argv[2]});
+        l = UpdateBuilNum(l, buildNum);
+    }
+
+//     for( auto l : tempF )
+//         std::cout << l << std::endl;
+
+    std::fstream fo {"fwid.cpp", std::ios::out };
+    for( auto l: tempF)
+        fo << l << std::endl;
+
+    fo.close();
 
 
     }
